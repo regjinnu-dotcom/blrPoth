e09f418d458e605f2e5c34938c114f8b7c311da4
Demi <demi@Demis-MacBook-Air.local>
2026-04-16 14:13:54 +0530

    js update

---
+++ config.js
@@ -0,0 +1,5 @@
+window.BLR_POTHOLE_CONFIG = {
+    supabaseUrl: "https://pnldwmsogeotfrjsqfnj.supabase.co",
+    supabaseAnonKey: "sb_publishable_xLZZxaayUPIkqNZ-CeXttQ_QtgwJjwO",
+    storageBucket: "report-photos",
+};
